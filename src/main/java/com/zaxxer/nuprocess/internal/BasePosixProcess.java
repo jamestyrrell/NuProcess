0545bf998609a0c5a9730adfc5f604676d377538
James Tyrrell <james@tyrrells.com>
2015-09-10 21:16:00 +1000

    Remove linux process reap and let BaseEventProcessor do its job

--- src/main/java/com/zaxxer/nuprocess/internal/BasePosixProcess.java
+++ src/main/java/com/zaxxer/nuprocess/internal/BasePosixProcess.java
@@ -25,26 +25,15 @@ import com.zaxxer.nuprocess.NuProcess;
 import com.zaxxer.nuprocess.NuProcessHandler;
 
 import java.nio.ByteBuffer;
-import java.nio.file.Path;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-import java.util.concurrent.Callable;
 import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.CyclicBarrier;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Executors;
-import java.util.concurrent.LinkedBlockingQueue;
-import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.ThreadFactory;
-import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
-
-import static com.zaxxer.nuprocess.internal.LibC.*;
 
 public abstract class BasePosixProcess implements NuProcess
 {
@@ -55,8 +44,6 @@ public abstract class BasePosixProcess implements NuProcess
 
    protected static final IEventProcessor<? extends BasePosixProcess>[] processors;
    protected static int processorRoundRobin;
-
-   private static ExecutorService linuxCwdExecutorService;
 
    protected IEventProcessor<? super BasePosixProcess> myProcessor;
    protected volatile NuProcessHandler processHandler;
@@ -91,32 +78,6 @@ public abstract class BasePosixProcess implements NuProcess
    private int remainingWrite;
    private int writeOffset;
 
-   private Pointer posix_spawn_file_actions;
-
-   // Launches threads under which changes to cwd do not affect the cwd of the process.
-   public static class LinuxCwdThreadFactory implements ThreadFactory {
-      private final AtomicInteger threadCount = new AtomicInteger(0);
-
-      @Override
-      public Thread newThread(final Runnable r) {
-         Runnable unshareCwdThenSpawn = new Runnable() {
-            @Override
-            public void run() {
-              // This makes a copy of the process's chroot, cwd, and umask and
-              // allows the thread to change any of those in a way that doesn't
-              // affect the rest of the process.
-              int rc = LinuxLibC.unshare(LinuxLibC.CLONE_FS);
-              // If this throws, then it bubbles up to whomever called ExecutorService.submit().
-              checkReturnCode(rc, "unshare(CLONE_FS) failed");
-              r.run();
-            }
-         };
-         Thread newThread = Executors.defaultThreadFactory().newThread(unshareCwdThenSpawn);
-         newThread.setName(String.format("NuProcessLinuxCwdChangeable-%d", threadCount.incrementAndGet()));
-         return newThread;
-      }
-   }
-
    static {
       IS_SOFTEXIT_DETECTION = Boolean.valueOf(System.getProperty("com.zaxxer.nuprocess.softExitDetection", "true"));
 
@@ -146,19 +107,6 @@ public abstract class BasePosixProcess implements NuProcess
                }
             }
          }));
-      }
-
-      if (IS_LINUX) {
-         ThreadPoolExecutor executor = new ThreadPoolExecutor(
-             /* corePoolSize */ numThreads,
-             /* maximumPoolSize */ numThreads,
-             /* keepAliveTime */ BaseEventProcessor.LINGER_TIME_MS, TimeUnit.MILLISECONDS,
-             /* workQueue */ new LinkedBlockingQueue<Runnable>(),
-             /* threadFactory */ new LinuxCwdThreadFactory(),
-             /* handler */ new ThreadPoolExecutor.DiscardPolicy());
-         // Allow going back down to 0 threads after LINGER_TIME_MS.
-         executor.allowCoreThreadTimeOut(true);
-         linuxCwdExecutorService = executor;
       }
    }
 
@@ -267,10 +215,10 @@ public abstract class BasePosixProcess implements NuProcess
    //                             Public methods
    // ************************************************************************
 
-   public NuProcess start(List<String> command, String[] environment, Path cwd)
+   public NuProcess start(List<String> command, String[] environment)
    {
       callPreStart();
-
+      
       String[] commands = command.toArray(new String[command.size()]);
 
       Pointer posix_spawn_file_actions = createPipes();
@@ -299,50 +247,11 @@ public abstract class BasePosixProcess implements NuProcess
          LibC.posix_spawnattr_setflags(posix_spawnattr, flags);
 
          IntByReference restrict_pid = new IntByReference();
-         StringArray commandsArray = new StringArray(commands);
-         StringArray environmentArray = new StringArray(environment);
-         if (cwd != null) {
-             if (IS_MAC) {
-                 rc = spawnOsxWithCwd(restrict_pid, commands[0], posix_spawn_file_actions, posix_spawnattr, commandsArray, environmentArray, cwd);
-             } else if (IS_LINUX) {
-                 rc = spawnLinuxWithCwd(restrict_pid, commands[0], posix_spawn_file_actions, posix_spawnattr, commandsArray, environmentArray, cwd);
-             } else {
-                 throw new RuntimeException("Platform does not support per-thread cwd override");
-             }
-         } else {
-             rc = LibC.posix_spawnp(restrict_pid, commands[0], posix_spawn_file_actions, posix_spawnattr, commandsArray, environmentArray);
-         }
-
-         pid = restrict_pid.getValue();
-
-         initializeBuffers();
-
-         // This is necessary on Linux because spawn failures are not reflected in the rc, and this will reap
-         // any zombies due to launch failure
-         if (IS_LINUX) {
-            IntByReference ret = new IntByReference();
-            rc = LibC.waitpid(pid, ret, LibC.WNOHANG);
-
-            if (rc != 0) {
-               int status = ret.getValue();
-               if (WIFEXITED(status)) {
-                  status = WEXITSTATUS(status);
-                  if (status == 127) {
-                     onExit(Integer.MIN_VALUE);
-                  }
-                  else {
-                     onExit(status);
-                  }
-               }
-               else if (WIFSIGNALED(status)) {
-                  onExit(WTERMSIG(status));
-               }
-
-               return null;
-            }
-         }
+         rc = LibC.posix_spawnp(restrict_pid, commands[0], posix_spawn_file_actions, posix_spawnattr, new StringArray(commands), new StringArray(environment));
 
          checkReturnCode(rc, "Invocation of posix_spawn() failed");
+
+         pid = restrict_pid.getValue();
 
          afterStart();
 
@@ -377,71 +286,6 @@ public abstract class BasePosixProcess implements NuProcess
       }
 
       return this;
-   }
-
-   private int spawnOsxWithCwd(
-      IntByReference restrict_pid,
-      String restrict_path,
-      Pointer file_actions,
-      Pointer /*const posix_spawnattr_t*/restrict_attrp,
-      StringArray /*String[]*/argv,
-      Pointer /*String[]*/envp,
-      Path cwd)
-   {
-     int cwdBufSize = 1024;
-     long peer = Native.malloc(cwdBufSize);
-     Pointer oldCwd = new Pointer(peer);
-     LibC.getcwd(oldCwd, cwdBufSize);
-     String newCwd = cwd.toAbsolutePath().toString();
-     int rc = LibC.SYSCALL.syscall(LibC.SYSCALL.SYS___pthread_chdir, newCwd);
-     checkReturnCode(rc, "syscall(SYS__pthread_chdir) failed to set current directory");
-
-     try {
-       return LibC.posix_spawnp(restrict_pid, restrict_path, file_actions, restrict_attrp, argv, envp);
-     } finally {
-       rc = LibC.SYSCALL.syscall(LibC.SYSCALL.SYS___pthread_chdir, oldCwd);
-       Native.free(Pointer.nativeValue(oldCwd));
-       checkReturnCode(rc, "syscall(SYS__pthread_chdir) failed to restore current directory");
-     }
-   }
-
-   private int spawnLinuxWithCwd(
-      final IntByReference restrict_pid,
-      final String restrict_path,
-      final Pointer file_actions,
-      final Pointer /*const posix_spawnattr_t*/restrict_attrp,
-      final StringArray /*String[]*/argv,
-      final Pointer /*String[]*/envp,
-      final Path cwd)
-   {
-     Future<Integer> setCwdThenSpawnFuture = linuxCwdExecutorService.submit(
-         new Callable<Integer>() {
-           @Override
-           public Integer call() {
-               // Set cwd in this thread, which has its cwd state disassociated from the rest of the process.
-               int rc = LibC.chdir(cwd.toAbsolutePath().toString());
-               // If this throws, it'll be wrapped in an ExecutionException and re-thrown on the thread
-               // which calls Future.get().
-               checkReturnCode(rc, "chdir() failed");
-               // posix_spawnp() will inherit the cwd of this thread.
-               //
-               // We don't bother restoring cwd, since this thread will either be destroyed or re-used
-               // later by the same executor, which will then chdir anyway.
-               return LibC.posix_spawnp(restrict_pid, restrict_path, file_actions, restrict_attrp, argv, envp);
-           }
-         });
-     try {
-       return setCwdThenSpawnFuture.get();
-     } catch (ExecutionException e) {
-       Throwable cause = e.getCause();
-       if (cause instanceof RuntimeException) {
-         throw (RuntimeException) cause;
-       } else {
-         throw new RuntimeException(cause);
-       }
-     } catch (InterruptedException e) {
-       throw new RuntimeException(e);
-     }
    }
 
    public int getPid()
@@ -676,13 +520,9 @@ public abstract class BasePosixProcess implements NuProcess
 
    private void afterStart()
    {
-      isRunning = true;
-   }
-
-   private void initializeBuffers()
-   {
       outClosed = false;
       errClosed = false;
+      isRunning = true;
 
       pendingWrites = new ConcurrentLinkedQueue<ByteBuffer>();
 
@@ -860,7 +700,7 @@ public abstract class BasePosixProcess implements NuProcess
       }
    }
 
-   private static void checkReturnCode(int rc, String failureMessage)
+   private void checkReturnCode(int rc, String failureMessage)
    {
       if (rc != 0) {
          throw new RuntimeException(failureMessage + ", return code: " + rc + ", last error: " + Native.getLastError());
